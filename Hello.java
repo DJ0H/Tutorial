f60d23a968f0cdf4511e65c89f2a3fbff0ea96b6
DJ0H <odajung02@gmail.com>
2021-12-01 18:02:58 +0900

    Add Text File [a.txt]

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/*
+	여러줄의 주석을 작성
+	작성일 : 1125
+	작성자 : 오다정
+*/
+
+// 주석(comment) 사람을 위해서 적음
+// 헬로 클래스 입니다.
+class Hello {
+	public static void main(String[] args) { // 메인 메소드(method) 프로그램의 시작점
+		System.out.println("Hello world!"); // 문장(Statement)
+		System.out.println("123"); // 절차대로 실행
+		System.out.println("abc"); // println은 출력 기능
+	}
+}
+
